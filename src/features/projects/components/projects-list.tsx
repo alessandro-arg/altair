8051c8e53eab1283a3af226dc98adc4fa4894781
Alessandro Argenziano <contact@alessandro-argenziano.com>
2026-02-06 12:59:54 +0100

    feat: working on the recent projects and on the importStatus of them

--- src/features/projects/components/projects-list.tsx
+++ src/features/projects/components/projects-list.tsx
@@ -1,6 +1,54 @@
 import { Spinner } from "@/components/ui/spinner";
 import { useProjectsPartial } from "../hooks/use-projects";
 import { Kbd } from "@/components/ui/kbd";
+import { Doc } from "../../../../convex/_generated/dataModel";
+import Link from "next/link";
+import { AlertCircleIcon, GlobeIcon, Loader2Icon } from "lucide-react";
+import { formatDistanceToNow } from "date-fns";
+import { FaGithub } from "react-icons/fa";
+
+const formatTimestamp = (timestamp: number) => {
+  return formatDistanceToNow(new Date(timestamp), { addSuffix: true });
+};
+
+const getProjectIcon = (project: Doc<"projects">) => {
+  if (project.importStatus === "completed") {
+    return <FaGithub className="size-3.5 text-muted-foreground" />;
+  }
+
+  if (project.importStatus === "failed") {
+    return <AlertCircleIcon className="size-3.5 text-muted-foreground" />;
+  }
+
+  if (project.importStatus === "importing") {
+    return (
+      <Loader2Icon className="size-3.5 text-muted-foreground animate-spin" />
+    );
+  }
+
+  return <GlobeIcon className="size-3.5 text-muted-foreground" />;
+};
+
+const ContinueCard = ({ data }: { data: Doc<"projects"> }) => {
+  return <div>Test</div>;
+};
+
+const ProjectItem = ({ data }: { data: Doc<"projects"> }) => {
+  return (
+    <Link
+      href={`/projects/${data._id}`}
+      className="text-sm text-foreground/60 font-medium hover:text-foreground py-1 flex items-center justify-between w-full group"
+    >
+      <div className="flex items-center gap-2">
+        {getProjectIcon(data)}
+        <span className="truncate">{data.name}</span>
+      </div>
+      <span className="text-xs text-muted-foreground group-hover:text-foreground/60 transition-colors">
+        {formatTimestamp(data.updatedAt)}
+      </span>
+    </Link>
+  );
+};
 
 interface ProjectsListProps {
   onViewAll: () => void;
@@ -13,12 +61,15 @@ export const ProjectsList = ({ onViewAll }: ProjectsListProps) => {
     return <Spinner className="size-4 text-ring" />;
   }
 
+  const [mostRecent, ...rest] = projects;
+
   return (
     <div className="flex flex-col gap-4">
-      {projects.length > 0 && (
+      <ContinueCard data={mostRecent} />
+      {rest.length > 0 && (
         <div className="flex flex-col gap-2">
           <div className="flex items-center justify-between gap-2">
-            <span className="text-sm text-muted-foreground">
+            <span className="text-xs text-muted-foreground">
               Recent Projects
             </span>
             <button className="flex items-center gap-2 text-muted-foreground text-xs hover:text-foreground transition-colors">
@@ -26,6 +77,11 @@ export const ProjectsList = ({ onViewAll }: ProjectsListProps) => {
               <Kbd className="bg-accent border">Ctrl + K</Kbd>
             </button>
           </div>
+          <ul className="flex flex-col">
+            {rest.map((project) => (
+              <ProjectItem key={project._id} data={project} />
+            ))}
+          </ul>
         </div>
       )}
     </div>
