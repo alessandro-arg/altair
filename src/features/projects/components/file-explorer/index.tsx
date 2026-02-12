652479a34733f1121a7113df9e9d368b609cd53c
Alessandro Argenziano <contact@alessandro-argenziano.com>
2026-02-12 14:46:03 +0100

    feat: working on the file and folder creation functions

--- src/features/projects/components/file-explorer/index.tsx
+++ src/features/projects/components/file-explorer/index.tsx
@@ -1,13 +1,44 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
-import { ChevronRightIcon } from "lucide-react";
+import {
+  ChevronRightIcon,
+  CopyMinusIcon,
+  FilePlusCornerIcon,
+  FolderPlusIcon,
+} from "lucide-react";
 import { useState } from "react";
 import { Id } from "../../../../../convex/_generated/dataModel";
 import { useProject } from "../../hooks/use-projects";
+import { Button } from "@/components/ui/button";
+import { useCreateFile, useCreateFolder } from "../../hooks/use-files";
+import { CreateInput } from "./create-input";
 
 export const FileExplorer = ({ projectId }: { projectId: Id<"projects"> }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [collapseKey, setCollapseKey] = useState(0);
+  const [creating, setCreating] = useState<"file" | "folder" | null>(null);
   const project = useProject(projectId);
+
+  const createFile = useCreateFile();
+  const createFolder = useCreateFolder();
+  const handleCreate = (name: string) => {
+    setCreating(null);
+
+    if (creating === "file") {
+      createFile({
+        projectId,
+        name,
+        content: "",
+        parentId: undefined,
+      });
+    } else {
+      createFolder({
+        projectId,
+        name,
+        parentId: undefined,
+      });
+    }
+  };
 
   return (
     <div className="h-full bg-sidebar">
@@ -26,7 +57,58 @@ export const FileExplorer = ({ projectId }: { projectId: Id<"projects"> }) => {
           <p className="text-xs uppercase line-clamp-1">
             {project?.name ?? "Loading..."}
           </p>
+          <div className="opacity-0 group-hover/project:opacity-100 transition-none duration-0 flex items-center gap-0.5 ml-auto">
+            <Button
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                setIsOpen(true);
+                setCreating("file");
+              }}
+              variant="highlight"
+              size="icon-xs"
+            >
+              <FilePlusCornerIcon className="size-3.5" />
+            </Button>
+            <Button
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                setIsOpen(true);
+                setCreating("folder");
+              }}
+              variant="highlight"
+              size="icon-xs"
+            >
+              <FolderPlusIcon className="size-3.5" />
+            </Button>
+            <Button
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                setCollapseKey((prev) => prev + 1);
+                setIsOpen(false);
+              }}
+              variant="highlight"
+              size="icon-xs"
+            >
+              <CopyMinusIcon className="size-3.5" />
+            </Button>
+          </div>
         </div>
+
+        {isOpen && (
+          <>
+            {creating && (
+              <CreateInput
+                type={creating}
+                level={0}
+                onSubmit={handleCreate}
+                onCancel={() => setCreating(null)}
+              />
+            )}
+          </>
+        )}
       </ScrollArea>
     </div>
   );
